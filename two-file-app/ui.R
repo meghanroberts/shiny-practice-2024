fc63d0f412e317256718d83a935964bf6b3e97f8
meghanroberts <meghanroberts@bren.ucsb.edu>
2024-01-26 13:58:38 -0800

    setting up the structure for two file app

--- two-file-app/ui.R
+++ two-file-app/ui.R
@@ -1,2 +1,68 @@
 # user interface ----
-ui<- fluidPage()
+ui<- navbarPage(
+  
+  title="LTER Animal Data Explorer",
+  
+  # (Page 1) intro tab pane; -----
+  tabPanel(title="About this page",
+           
+           "background info goes here"
+           
+  ), #END pg1
+  # (Page 2) data vis tab panel ----
+  tabPanel(title="Explore the data", 
+           
+           #tabsetpanel to contain tabs for data vis ----
+           tabsetPanel(
+             
+             #trout tab panel ----
+             tabPanel(title="Trout",
+                      
+                      #trout side bar layout ----
+                      sidebarLayout(
+                        
+                        #trout side bar panel ----
+                        sidebarPanel(
+                          
+                          "trout inputs will live here"
+                          
+                        ),#END trout side bar panel
+                        
+                        #trout main panel ----
+                        mainPanel(
+                          
+                          "trout outputs here"
+                          
+                        )#END trout main panel
+                      ) #END Trout side bar layout
+                      
+             ), #END trout tab panel
+             #penguin tab panel ----
+             tabPanel(title="Penguin",
+                      
+                      #penguin side bar layout ----
+                      sidebarLayout(
+                        
+                        #penguin side bar panel ----
+                        sidebarPanel(
+                          
+                          "penguins inputs live here"
+                          
+                        ), #END penguin side bar panel
+                        
+                        #penguin main panel -----
+                        mainPanel(
+                          
+                          "penguin outputs here"
+                          
+                        ) #END penguin main panel
+                        
+                      ) #END penguin side bar layout
+                      
+             ) #END penguin tab panel
+             
+           )#END tabsetPanel
+           
+  ) #END pg 2
+  
+)#END navbar page
