c5328f0e404b91473dfe74e9aac454ad37cc49bc
meghanroberts <meghanroberts@bren.ucsb.edu>
2024-02-02 11:24:49 -0800

    structure of ui is complete

--- shiny-dashboard/ui.R
+++ shiny-dashboard/ui.R
@@ -16,7 +16,7 @@ sidebar<-dashboardSidebar(
              tabName = "welcome", #used to identify this sidetab later on
              icon=icon("star")), 
     menuItem(text="Dashboard", 
-             tabName = "dashboar", 
+             tabName = "dashboard", 
              icon=icon("gauge"))
   ) #END sidebarMenu
   
@@ -24,7 +24,76 @@ sidebar<-dashboardSidebar(
 )
 
 #dashboard body ------------------------------------
-body<-dashboardBody()
+body<-dashboardBody(
+  
+  #tab items (container function)---------------
+  tabItems(
+    
+    #welcome tabeItem --------------
+    tabItem(
+      tabName = "welcome", #from above
+        
+      #left-hand column
+      column(width=6, 
+             
+             #background info box
+             box(width=NULL, #takes on width of the column
+                 "background info here"
+                 ) #END background box
+             
+             ), #END column
+      
+      #right-hand column
+      column(width=6, 
+             
+             #create two boxes set on top of one another
+             #fluidrow one------------------------
+             fluidRow(
+               
+               #citation box
+               box(width=NULL, 
+                   "citation here"
+                   )#END citation box
+               
+             ),#END fluidrow one
+             
+             #fluidrow two --------------------------
+             fluidRow(
+               
+               #disclaimer box
+               box(width=NULL, 
+                   "disclaimer here"
+                   ) #END disclaimer box
+             ) #END fluidrow two
+             
+             )#END column right
+      
+    ), #END welcome tab item
+    
+    #dashboard tabeItem --------------
+    tabItem(
+      tabName = "dashboard", #from above
+      
+      #fluid row ------------
+      fluidRow(
+        
+        #input box -----------
+        box(width = 4, 
+            "slider inputs here"
+            ), #END input box
+        #map box --------------
+        box(width=8, 
+            "map here"
+            ) #END map here
+        
+      )#END Fluidrow
+      
+    ) #END dashboard tab item
+    
+  )#END tab items
+  
+)#END dashboard body
 
-#cmobine them ----------------------------------------
+#combine them ----------------------------------------
 dashboardPage(header, sidebar, body)
+
